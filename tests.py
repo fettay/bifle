82ccf22b1e970146eb8fd9e0072195494cef0e23
Joe Shamah <joe@mrsomeone.com>
2016-02-11 21:18:22 +0200

    Adding Drone and Item classes.

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from include_functions import *
+from drone import Drone
+from item import Item
 
 
 if __name__ == "__main__":
@@ -6,4 +8,17 @@ if __name__ == "__main__":
    read_input_file('data/busy_day.in')
    read_input_file('data/mother_of_all_warehouses.in')
    read_input_file('data/redundancy.in')
+
+   test_item = Item(123, 100)
+   test_item2 = Item(123, 1)
+   test_drone = Drone(100)
+   print test_drone.location
+   test_drone.move_to([10, 10])
+   print test_drone.location
+
+   test_drone.add_item(test_item, None)
+   print test_drone.current_weight
+   test_drone.deliver_item(test_item, None)
+   test_drone.add_item(test_item2, None)
+   print test_drone.current_weight
 
